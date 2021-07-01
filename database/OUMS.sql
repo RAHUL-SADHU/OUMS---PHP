2a5026b4c606017f175038c972af5380b0b8d028
Rahul Sadhu <rahul.sadhu@openxcelltechnolabs.com>
2021-07-01 22:02:36 +0530

    sql file update

--- database/OUMS.sql
+++ database/OUMS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 26, 2021 at 09:21 AM
+-- Generation Time: Jul 01, 2021 at 06:31 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 5.6.39
 
@@ -43,8 +43,7 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
 (1, 'Rahul', 'Sadhu', 'r@r.com', '1111', 'Admin'),
-(3, 'MAYA', 'ACHaraya', 'm@m.com', '1111', 'Admin'),
-(7, 'Bhavesh', 'Sadhu', 'bhavesh@b.com', '123456', 'Admin');
+(3, 'MAYA', 'ACHaraya', 'm@m.com', '1111', 'Admin');
 
 -- --------------------------------------------------------
 
@@ -68,74 +67,48 @@ CREATE TABLE `attendance` (
 
 INSERT INTO `attendance` (`id`, `department_id`, `semester`, `subject_id`, `attendance_date`, `student_id`, `present`) VALUES
 (88, 26, '1', 1, '2021-05-22', 1, 0),
-(89, 26, '1', 1, '2021-05-22', 4, 0),
 (90, 26, '1', 1, '2021-05-22', 6, 0),
 (91, 26, '1', 1, '2021-05-20', 1, 0),
-(92, 26, '1', 1, '2021-05-20', 4, 0),
 (93, 26, '1', 1, '2021-05-20', 6, 0),
 (94, 26, '1', 1, '2021-05-29', 1, 0),
-(95, 26, '1', 1, '2021-05-29', 4, 0),
 (96, 26, '1', 1, '2021-05-29', 6, 0),
 (97, 26, '1', 1, '2021-05-31', 1, 1),
-(98, 26, '1', 1, '2021-05-31', 4, 1),
 (99, 26, '1', 1, '2021-05-31', 6, 1),
-(100, 26, '1', 4, '2021-05-22', 1, 1),
-(101, 26, '1', 4, '2021-05-22', 4, 1),
-(102, 26, '1', 4, '2021-05-22', 6, 1),
-(103, 26, '1', 4, '2021-07-23', 1, 1),
-(104, 26, '1', 4, '2021-07-23', 4, 1),
-(105, 26, '1', 4, '2021-07-23', 6, 1),
 (106, 26, '1', 1, '2021-06-26', 1, 1),
-(107, 26, '1', 1, '2021-06-26', 4, 1),
 (108, 26, '1', 1, '2021-06-26', 6, 1),
 (109, 26, '1', 2, '2021-05-01', 1, 1),
-(110, 26, '1', 2, '2021-05-01', 4, 1),
 (111, 26, '1', 2, '2021-05-01', 6, 1),
 (112, 26, '1', 3, '2021-03-13', 1, 0),
-(113, 26, '1', 3, '2021-03-13', 4, 0),
 (114, 26, '1', 3, '2021-03-13', 6, 0),
 (115, 26, '1', 3, '2021-03-14', 1, 0),
-(116, 26, '1', 3, '2021-03-14', 4, 0),
 (117, 26, '1', 3, '2021-03-14', 6, 0),
 (118, 26, '1', 3, '2021-03-15', 1, 0),
-(119, 26, '1', 3, '2021-03-15', 4, 0),
 (120, 26, '1', 3, '2021-03-15', 6, 0),
 (121, 26, '1', 1, '2021-01-22', 1, 1),
-(122, 26, '1', 1, '2021-01-22', 4, 1),
 (123, 26, '1', 1, '2021-01-22', 6, 1),
 (124, 26, '1', 1, '2020-10-16', 1, 1),
-(125, 26, '1', 1, '2020-10-16', 4, 1),
 (126, 26, '1', 1, '2020-10-16', 6, 1),
 (127, 26, '1', 1, '2020-10-15', 1, 1),
-(128, 26, '1', 1, '2020-10-15', 4, 1),
 (129, 26, '1', 1, '2020-10-15', 6, 1),
 (130, 26, '1', 1, '2020-01-10', 1, 0),
-(131, 26, '1', 1, '2020-01-10', 4, 0),
 (132, 26, '1', 1, '2020-01-10', 6, 0),
 (133, 26, '1', 1, '2020-01-09', 1, 0),
-(134, 26, '1', 1, '2020-01-09', 4, 0),
 (135, 26, '1', 1, '2020-01-09', 6, 0),
 (136, 26, '1', 1, '2020-01-04', 1, 0),
-(137, 26, '1', 1, '2020-01-04', 4, 0),
 (138, 26, '1', 1, '2020-01-04', 6, 0),
 (139, 26, '1', 1, '2019-08-16', 1, 0),
-(140, 26, '1', 1, '2019-08-16', 4, 1),
 (141, 26, '1', 1, '2019-08-16', 6, 0),
 (142, 26, '1', 1, '2019-08-03', 1, 0),
-(143, 26, '1', 1, '2019-08-03', 4, 0),
 (144, 26, '1', 1, '2019-08-03', 6, 1),
 (145, 26, '1', 1, '2023-02-26', 1, 1),
-(146, 26, '1', 1, '2023-02-26', 4, 1),
 (147, 26, '1', 1, '2023-02-26', 6, 1),
 (148, 26, '1', 1, '2023-02-27', 1, 1),
-(149, 26, '1', 1, '2023-02-27', 4, 0),
 (150, 26, '1', 1, '2023-02-27', 6, 1),
 (151, 26, '1', 3, '2021-05-22', 1, 0),
-(152, 26, '1', 3, '2021-05-22', 4, 0),
 (153, 26, '1', 3, '2021-05-22', 6, 0),
 (154, 26, '1', 3, '2021-05-31', 1, 0),
-(155, 26, '1', 3, '2021-05-31', 4, 0),
-(156, 26, '1', 3, '2021-05-31', 6, 1);
+(156, 26, '1', 3, '2021-05-31', 6, 1),
+(157, 27, '1', 5, '2021-07-09', 6, 1);
 
 -- --------------------------------------------------------
 
@@ -162,7 +135,8 @@ CREATE TABLE `book` (
 
 INSERT INTO `book` (`book_id`, `book_no`, `name`, `author`, `quantity`, `rack_no`, `row_no`, `book_type`, `department_id`, `description`) VALUES
 (2, '12034', 'Java', 'TaxMa', 12, '13', '52', 'Story', 26, 'testde'),
-(3, '1254', 'Oracle', 'Herbert schildt  ', 12, '1', '2', 'Academic', 26, 'test');
+(3, '1254', 'Oracle', 'Herbert schildt  ', 12, '1', '2', 'Academic', 26, 'test'),
+(4, '1245', 'Network Security', 'TaxMax', 50, '1', '4', 'Academic', 26, 'test');
 
 -- --------------------------------------------------------
 
@@ -205,8 +179,7 @@ INSERT INTO `borrow_book` (`id`, `department_id`, `semester`, `student_id`, `iss
 (15, 26, 1, 1, '2021-04-17', 2, 12, '2021-04-16', 12, 'borrow'),
 (16, 26, 1, 1, '2021-04-17', 2, 12, '2021-04-16', 12, 'borrow'),
 (17, 26, 1, 1, '2021-04-17', 2, 12, '2021-04-29', 1, 'borrow'),
-(18, 26, 1, 4, '2021-04-18', 3, 12, '2021-04-22', 2, 'borrow'),
-(19, 26, 1, 4, '2021-04-18', 2, 12, '2021-04-30', 12, 'borrow');
+(20, 26, 1, 6, '2021-07-02', 2, 1, '2021-07-03', 0, 'Borrowed');
 
 -- --------------------------------------------------------
 
@@ -231,7 +204,7 @@ INSERT INTO `department` (`id`, `name`, `code`, `description`, `year`, `crated_a
 (26, 'BCA', '12', 'Test', 1241, '2021-03-01 15:57:46'),
 (27, 'MCA', '12541', 'test', 2021, '2021-03-24 15:20:39'),
 (28, 'B.COM', '1254', 'B.COM Description', 2009, '2021-04-20 04:41:37'),
-(29, 'M.COM', '1222', 'Master of commerce', 2001, '2021-04-20 04:42:07');
+(30, 'M.COM', '1214', 'Test', 2006, '2021-07-01 15:35:30');
 
 -- --------------------------------------------------------
 
@@ -257,10 +230,8 @@ CREATE TABLE `exam` (
 
 INSERT INTO `exam` (`exam_id`, `department_id`, `semester`, `exam`, `subject_id`, `student_id`, `written`, `presentation`, `practical`) VALUES
 (1, 26, 1, 'Midterm Exam', 1, 1, 12, 21, 50),
-(2, 26, 1, 'Midterm Exam', 1, 4, 30, 20, 0),
 (3, 26, 1, 'Midterm Exam', 1, 6, 0, 0, 0),
 (4, 26, 1, 'Final Exam', 1, 1, 50, 27, 30),
-(5, 26, 1, 'Final Exam', 1, 4, 50, 55, 35),
 (6, 26, 1, 'Final Exam', 1, 6, 40, 55, 85);
 
 -- --------------------------------------------------------
@@ -285,7 +256,8 @@ CREATE TABLE `fees` (
 
 INSERT INTO `fees` (`fees_id`, `student_id`, `payableAmount`, `lateFee`, `paidAmount`, `dueAmount`, `payDate`) VALUES
 (1, 1, '1500', '0', '1000', '500', '2021-05-01'),
-(2, 1, '2000', '0', '2000', '0', '2021-05-06');
+(2, 1, '2000', '0', '2000', '0', '2021-05-06'),
+(3, 1, '1500', '0', '0', '1500', '2021-07-09');
 
 -- --------------------------------------------------------
 
@@ -308,7 +280,7 @@ CREATE TABLE `institute` (
 --
 
 INSERT INTO `institute` (`institute_id`, `name`, `establish`, `website`, `email`, `phone_number`, `address`) VALUES
-(1, 'Government Engineering Collage Gandhinagar', 1998, 'https://nandkunvarbamahilacollege.com/', 'nmcbhavnagar@gmail.com', 2147483647, 'Devraj nagar - 2, Saher farti sadak, Near Shivaji Circle, Ghogha Road, Bhavnagar.\r\n\r\n\"\"\"');
+(1, 'Government Engineering Collage Gandhinagar', 2000, 'https://nandkunvarbamahilacollege.com/', 'nmcbhavnagar@gmail.com', 2147483647, 'Devraj nagar - 2, Saher farti sadak, Near Shivaji Circle, Ghogha Road, Bhavnagar.\r\n\r\n\"\"\"');
 
 -- --------------------------------------------------------
 
@@ -344,10 +316,10 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`id`, `department_id`, `semester`, `first_name`, `last_name`, `middle_name`, `student_id`, `blood_group`, `gender`, `religion`, `nationality`, `bod`, `phone`, `father_name`, `father_mo`, `mother_name`, `mother_mo`, `present_address`, `permanent_address`, `profile_image`) VALUES
-(1, 28, 1, 'first_namesdfs', 'Sadhu', 'As', 584, 'A+', 'male', 'Hindu', 'Indian', '2021-03-11', '08487054954', 'Test', '08487054954', 'testt', '08487054954', 'Openxcell', 'TA VEJALPUR DIST AHMEDABAD ', 'AndroidR-Google.png'),
-(4, 27, 1, 'first_namesdf', 'Sadhu', 's', 4545, 'A+', 'male', 'Hindu', 'Indian', '2020-12-18', '08487054954', '44fsdfs', '84754', 'sdfsdf', '46455565sdfsdf', 'Openxcell', 'TA VEJALPUR DIST AHMEDABAD', 'images (1).jpeg'),
+(1, 26, 1, 'first_namesdfs', 'Sadhu', 'As', 584, 'A+', 'male', 'Hindu', 'Indian', '2021-03-11', '08487054954', 'Test', '08487054954', 'testt', '08487054954', 'Openxcell', 'TA VEJALPUR DIST AHMEDABAD  ', 'AndroidR-Google.png'),
 (5, 27, 5, 'Rahul', 'Sadhu', 'A', 124587, 'O+', 'male', 'Hindu', 'Indian', '1994-03-21', '8487054954', 'AshokBhai', '85871', 'Ilaben', '887544', 'Bakrol', 'test         ', 'FB_IMG_1585833060339.jpg'),
-(6, 27, 1, 'Tome', 'Johnshan', 'John', 1547, 'A+', 'male', 'Hindu', 'Indian', '2021-04-10', '9876543210', 'John', '9876543210', 'Maria', '9876543210', '739 Ridge Street\r\nMassillon, OH 44646', '739 Ridge Street\r\nMassillon, OH 44646  ', 'unnamed.jpg');
+(6, 27, 1, 'Tome', 'Johnshan', 'John', 1547, 'A+', 'male', 'Hindu', 'Indian', '2021-04-10', '9876543210', 'John', '9876543210', 'Maria', '9876543210', '739 Ridge Street\r\nMassillon, OH 44646', '739 Ridge Street\r\nMassillon, OH 44646  ', 'unnamed.jpg'),
+(7, 28, 1, 'Ram', 'Raghuvir', 'Dasharath', 124, 'A+', 'male', 'Hindu', 'Indian', '2020-09-11', '08487054954', 'Dasharath', '987546257', 'Kausalya', '87415874785', 'TO BAKROL, DHOLKA ROAD', 'TA VEJALPUR DIST AHMEDABAD', 'unnamed.jpg');
 
 -- --------------------------------------------------------
 
@@ -372,7 +344,7 @@ INSERT INTO `subject` (`subject_id`, `subject_name`, `subject_code`, `subject_cr
 (1, 'Java', 120, 12, 26, 1),
 (2, 'C++', 123, 12, 26, 1),
 (3, 'DBMS', 105, 12, 26, 1),
-(4, 'Oracle', 154, 12, 26, 1);
+(5, 'Advance Java', 254, 12, 27, 1);
 
 --
 -- Indexes for dumped tables
@@ -389,9 +361,9 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `attendance`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `subject` (`subject_id`),
+  ADD KEY `d` (`department_id`),
   ADD KEY `student` (`student_id`),
-  ADD KEY `d` (`department_id`);
+  ADD KEY `subject` (`subject_id`);
 
 --
 -- Indexes for table `book`
@@ -464,25 +436,25 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=157;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=158;
 
 --
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `book_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `book_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `borrow_book`
 --
 ALTER TABLE `borrow_book`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `exam`
@@ -494,7 +466,7 @@ ALTER TABLE `exam`
 -- AUTO_INCREMENT for table `fees`
 --
 ALTER TABLE `fees`
-  MODIFY `fees_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `fees_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `institute`
@@ -506,13 +478,13 @@ ALTER TABLE `institute`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `subject_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `subject_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -522,9 +494,9 @@ ALTER TABLE `subject`
 -- Constraints for table `attendance`
 --
 ALTER TABLE `attendance`
-  ADD CONSTRAINT `d` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`),
-  ADD CONSTRAINT `student` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`),
-  ADD CONSTRAINT `subject` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
+  ADD CONSTRAINT `d` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `subject` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `borrow_book`
